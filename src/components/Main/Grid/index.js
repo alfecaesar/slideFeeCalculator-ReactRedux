fab38e3b155124c81b08df634c41649a4cfec9f1
Jade Bautista <edaj.bautista@gmail.com>
2018-07-18 15:30:38 +0800

    Income slider user modal

--- src/components/Main/Grid/index.js
+++ src/components/Main/Grid/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Input } from "@progress/kendo-react-inputs";
-import { DateInput } from "@progress/kendo-react-dateinputs";
-import { AgGridReact } from "ag-grid-react";
 
-import IncomeSource from "../IncomeSource/index";
-import FontAwesome from "react-fontawesome";
+import { AgGridReact } from "ag-grid-react";
+import { Button } from "@progress/kendo-react-buttons";
+
+import UserForm from "../UserForm";
+
 import moment from "moment";
 import isEmpty from "lodash.isempty";
 
@@ -17,12 +17,14 @@ class PatientGrid extends Component {
         this.state = {
             gridData: [],
             id: 0,
-            patientname: "",
+            firstname: "",
+            lastname:"",
+            gender:"",
             dob: new Date(),
             mrnumber: "",
             relationship: "",
             rowSelection: "multiple",
-            open: false
+            openUserForm: false
         };
     }
 
@@ -31,21 +33,28 @@ class PatientGrid extends Component {
     };
 
     onChangeText = e => {
+        console.log(e);
         this.setState({
             [e.target.props.name]: e.target.value
         });
     };
 
     onAddPatient = e => {
-        const { patientname, dob, mrnumber, relationship, id } = this.state;
+        const { firstname,lastname, dob, mrnumber, relationship, id,gender } = this.state;
         const dateMoment = moment.utc(dob).format("MM/DD/YYYY");
         const Id = id + 1;
-
+        console.log("firstname:" + firstname );
+        console.log("lastname:" + lastname );
+        console.log("dob:" + dob );
+        console.log("mrnumber:" + mrnumber );
+        console.log("relationship:" + relationship );
+        console.log("gender:" + gender );
         if (
-            isEmpty(patientname) ||
+            isEmpty(firstname) ||
+            isEmpty(lastname) ||
             isEmpty(dateMoment) ||
             isEmpty(mrnumber) ||
-            isEmpty(relationship)
+            isEmpty(relationship) || isEmpty(gender)
         ) {
             alert("All fields are required");
 
@@ -54,7 +63,7 @@ class PatientGrid extends Component {
 
         const data = {
             Id,
-            PatientName: patientname,
+            PatientName: firstname +" "+ lastname,
             Dob: dateMoment,
             MRNumber: mrnumber,
             Relationship: relationship
@@ -64,11 +73,14 @@ class PatientGrid extends Component {
 
         this.setState({
             gridData: [...this.state.gridData, data],
-            patientname: "",
+            firstname: "",
+            lastname:"",
+            gender:"",
             dob: new Date(),
             mrnumber: "",
             relationship: "",
-            id: Id
+            id: Id,
+            openUserForm: false
         });
     };
 
@@ -93,11 +105,23 @@ class PatientGrid extends Component {
     };
 
     onCloseModal = () => {
-        this.setState({ open: false });
+        this.setState({ openUserForm: false });
+    };
+
+    onOpenModal = () => {
+        this.setState({ openUserForm: true });
     };
 
     render() {
-        const { patientname, dob, mrnumber, relationship } = this.state;
+        const {
+            firstname,
+            lastname,
+            gender,
+            dob,
+            mrnumber,
+            relationship,
+            openUserForm
+        } = this.state;
         const columnDefs = [
             { headerName: "PatientName", field: "PatientName" },
             { headerName: "Date of Birth", field: "Dob" },
@@ -110,12 +134,24 @@ class PatientGrid extends Component {
 
         return (
             <div>
-                <IncomeSource
-                    open={this.state.open}
-                    onCloseModal={this.onCloseModal}
-                    style={{ width: "700px", height: "300px" }}
+                <UserForm
+                    firstname={firstname}
+                    lastname={lastname}
+                    gender={gender}
+                    dob={dob}
+                    mrnumber={mrnumber}
+                    relationship={relationship}
+                    open={openUserForm}
+                    onAddPatient={this.onAddPatient}
+                    onChangeText={this.onChangeText}
+                    changeDate={this.changeDate}
                 />
-                <button onClick={this.onRemoveSelected.bind(this)}>
+                <button onClick={this.onOpenModal}>Add Patient</button>
+
+                <button
+                    onClick={this.onRemoveSelected.bind(this)}
+                    style={{ float: "right" }}
+                >
                     Remove Selected
                 </button>
                 <br />
@@ -134,75 +170,6 @@ class PatientGrid extends Component {
                     />
                 </div>
                 <br />
-
-                <div
-                    className="form-container"
-                    style={{
-                        display: "flex",
-                        width: "110%"
-                    }}
-                >
-                    <div
-                        className="patientname-container"
-                        style={{ display: "inline-block", width: "25%" }}
-                    >
-                        <Input
-                            label="Patient Name"
-                            name="patientname"
-                            value={patientname}
-                            onChange={this.onChangeText}
-                        />
-                    </div>
-                    <div
-                        className="dob-container"
-                        style={{ display: "inline-block", width: "25%" }}
-                    >
-                        <DateInput
-                            label="Date of birth"
-                            value={dob}
-                            onChange={this.changeDate}
-                        />
-                    </div>
-                    <div
-                        className="mrnumber-container"
-                        style={{ display: "inline-block", width: "25%" }}
-                    >
-                        <Input
-                            label="MR #"
-                            value={mrnumber}
-                            width="100%"
-                            name="mrnumber"
-                            onChange={this.onChangeText}
-                        />
-                    </div>
-
-                    <div
-                        className="relationship-container"
-                        style={{ display: "inline-block", width: "25%" }}
-                    >
-                        <Input
-                            label="Relationship"
-                            value={relationship}
-                            width="100%"
-                            name="relationship"
-                            onChange={this.onChangeText}
-                        />
-                    </div>
-                    <div
-                        className="addpatient-container"
-                        style={{ display: "inline-block", width: "10%" }}
-                    >
-                        <FontAwesome
-                            name="plus-circle"
-                            style={{
-                                fontSize: "30px",
-                                cursor: "pointer",
-                                marginTop: "15px"
-                            }}
-                            onClick={this.onAddPatient}
-                        />
-                    </div>
-                </div>
             </div>
         );
     }
--- src/components/Main/UserForm/index.js
+++ src/components/Main/UserForm/index.js
@@ -1,10 +1,93 @@
 import React from "react";
 import Modal from "react-responsive-modal";
-import { Button } from "@progress/kendo-react-buttons";
 
+import { Button } from "@progress/kendo-react-buttons";
+import { Input } from "@progress/kendo-react-inputs";
+import { ComboBox } from "@progress/kendo-react-dropdowns";
+import { DatePicker } from '@progress/kendo-react-dateinputs';
+
+import FontAwesome from "react-fontawesome";
+import './UserForm.css';
 class UserForm extends React.Component {
     render() {
-        return <Modal open={open} center style={style} showCloseIcon={false} />;
+        const {
+            firstname,
+            lastname,
+            dob,
+            mrnumber,
+            gender,
+            relationship,
+            onChangeText,
+            changeDate,
+            open,
+            style,
+            onAddPatient
+        } = this.props;
+
+        return (
+            <Modal open={open} center style={style} showCloseIcon={true}>
+                <div
+                    className="form-container"
+                    style={{
+                        width: "500px"
+                    }}
+                >
+                    <div className="firstname-container">
+                        <label>First Name:</label>
+                        <br />
+                        <Input
+                            name="firstname"
+                            value={firstname}
+                            onChange={onChangeText}
+                            style={{width:"100%"}}
+                        />
+                    </div>
+                    <br />
+                    <div className="lastname-container">
+                        <label>Last Name:</label>
+                        <br />
+                        <Input
+                            name="lastname"
+                            value={lastname}
+                            onChange={onChangeText}
+                            style={{width:"100%"}}
+                        />
+                    </div>
+                    <br />
+                    <div className="mrnumber-container">
+                        <label>MR #:</label>
+                        <br />
+                        <Input
+                            value={mrnumber}
+                            name="mrnumber"
+                            onChange={onChangeText}
+                            style={{width:"100%"}}
+                        />
+                    </div>
+                    <br />
+                    <div className="gender-container">
+                        <label>Gender:</label>
+                        <br />
+                        <ComboBox name="gender" data={['Male', 'Female']} value={gender} style={{width:"100%"}} onChange={onChangeText}/>
+                    </div>
+                    <br />
+                    <div className="dob-container">
+                        <label>Date of Birth:</label>
+                        <DatePicker name="dob" onChange={changeDate}/>
+                        <br />
+                    </div>
+                    
+                    <br />
+                    <div className="relationship-container">
+                        <label>Relationship:</label>
+                        <br />
+                        <ComboBox name="relationship" value={relationship} onChange={onChangeText} data={["Head", "Father", "Mother", "Daughter", "Son", "Grandfather", "Grandmother", "Grandchild", "Cousin"]} style={{width:"100%"}}/>
+                    </div>
+                    <br/>
+                    <Button primary onClick={onAddPatient}>Submit</Button>
+                </div>
+            </Modal>
+        );
     }
 }
 
